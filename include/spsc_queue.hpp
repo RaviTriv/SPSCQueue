44235d4cbb9c98cfd4f0724f3412f57283ab2511
Ravi <trivediravi561@gmail.com>
2025-12-17 17:20:43 -0500

    basic ring buffer

--- include/spsc_queue.hpp
+++ include/spsc_queue.hpp
@@ -1,15 +1,44 @@
 #pragma once
 
+#include <array>
 #include <cstddef>
 
 namespace spsc {
-
 template <typename T, std::size_t Capacity> class Queue {
-
 public:
   Queue() = default;
 
-private:
-};
+  bool push(const T &item) {
+    if (full()) {
+      return false;
+    }
+    buffer_[tail_] = item;
+    tail_ = (tail_ + 1) % Capacity;
+    return true;
+  }
 
+  bool pop(T &item) {
+    if (empty()) {
+      return false;
+    }
+    item = buffer_[head_];
+    head_ = (head_ + 1) % Capacity;
+    return true;
+  }
+
+  bool empty() const { return head_ == tail_; }
+  bool full() const { return (tail_ + 1) % Capacity == head_; }
+  static constexpr std::size_t capacity() { return Capacity; };
+  std::size_t size() const {
+    if (tail_ >= head_) {
+      return tail_ - head_;
+    }
+    return Capacity - head_ + tail_;
+  };
+
+private:
+  std::array<T, Capacity> buffer_;
+  std::size_t head_ = 0;
+  std::size_t tail_ = 0;
+};
 } // namespace spsc
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include <spsc_queue.hpp>
 
 int main() {
-  spsc::Queue<int, 1024> q;
+  spsc::Queue<int, 5> q;
+  for (int i = 0; i < 5; i++) {
+    q.push(i);
+  }
+
+  while (!q.empty()) {
+    int item;
+    q.pop(item);
+    printf("%d\n", item);
+  }
+
   return 0;
 }
