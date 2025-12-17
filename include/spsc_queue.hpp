fca5f1695e2a22058bfb7b4f62176fce4636d606
Ravi <trivediravi561@gmail.com>
2025-12-17 08:52:14 -0500

    setup cmake

---
+++ include/spsc_queue.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <cstddef>
+
+namespace spsc {
+
+template <typename T, std::size_t Capacity> class Queue {
+
+public:
+  Queue() = default;
+
+private:
+};
+
+} // namespace spsc
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <spsc_queue.hpp>
+
+int main() {
+  spsc::Queue<int, 1024> q;
+  return 0;
+}
