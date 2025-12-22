dac813980c9cc9d07472f84182af14ee4071aa67
Ravi <trivediravi561@gmail.com>
2025-12-22 17:13:35 -0500

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,38 @@
+#include <cassert>
+#include <cstdio>
 #include <spsc_queue.hpp>
+#include <thread>
 
 int main() {
-  spsc::Queue<int, 5> q;
-  for (int i = 0; i < 5; i++) {
-    q.push(i);
-  }
+  constexpr int NUM_ITEMS = 100000000;
+  spsc::Queue<int, 1024> q;
 
-  while (!q.empty()) {
-    int item;
-    q.pop(item);
-    printf("%d\n", item);
-  }
+  std::thread producer([&q]() {
+    for (int i = 0; i < NUM_ITEMS; i++) {
+      while (!q.push(i)) {
+      }
+    }
+  });
 
+  std::thread consumer([&q]() {
+    int expected = 0;
+    while (expected < NUM_ITEMS) {
+      int val;
+      if (q.pop(val)) {
+        if (val != expected) {
+          printf("ERROR: Expected %d but got %d\n", expected, val);
+          printf("Not synced correctly\n");
+          exit(1);
+        }
+        expected++;
+      }
+    }
+  });
+
+  producer.join();
+  consumer.join();
+
+  assert(q.empty());
+  printf("Transferred %d items successfully.\n", NUM_ITEMS);
   return 0;
 }
